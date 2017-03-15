52a341e4f2cea5b6a898b56b90a8e00f0533738f
Andrew Rice <technicallyrice27@gmail.com>
2017-03-15 18:16:07 -0400

    Check validity of link and creates necessary directories

--- getSyosetu.py
+++ getSyosetu.py
@@ -4,24 +4,32 @@
 import requests, sys, os
 from bs4 import BeautifulSoup as bs
 
-# get url of first chapter from command line and make BeautifulSoup
-
+# make sure url is included
 if not sys.argv[1]:
     print("Please include link to first chapter as argument.")
     sys.exit()
 
+# make sure url returns a response and create a soup if it does
 res = requests.get(sys.argv[1])
 res.raise_for_status() # make sure it finds it otherwise exits
 soup = bs(res.content, "lxml")
 
-# make directory named DownloadedNovels in current dir and make new
-# dirs in that dir to save novels
+# check if DownloadedNovels dir exists, if not make it
+if not os.path.exists('DownloadedNovels'):
+    os.makedirs('DownloadedNovels')
+
+# make new directory in DownloadedNovels if necessary
 novel_title = soup.select('title')[0].get_text().split(" ")[0]
 chapter_title = soup.select('.novel_subtitle')[0].get_text()
 chapter_number = soup.select('#novel_no')[0].get_text().split("/")[0]
 chapter_total = soup.select('#novel_no')[0].get_text().split("/")[1]
-print("chapter_number = {}".format(chapter_number))
-print("chapter_total = {}".format(chapter_total))
+if int(chapter_number) < 10:
+    chapter_number = '00' + chapter_number
+elif int(chapter_number) < 100:
+    chapter_number = '0' + chapter_number
+
+if not os.path.exists('DownloadedNovels/' + novel_title):
+    os.makedirs('DownloadedNovels/' + novel_title)
 # download each page and write chapter number as Chapter 001 - {title}.txt
 
 # loop until final chapter downloaded
